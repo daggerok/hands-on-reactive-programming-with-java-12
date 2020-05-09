91f9e0ec4a0146bfdab23def964716024526ebf6
Maksim Kostromin <daggerok@gmail.com>
2020-05-09 18:08:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hands-On Reactive Programming with Java 12 [![Build Status](https://travis-ci.org/daggerok/hands-on-reactive-programming-with-java-12.svg?branch=master)](https://travis-ci.org/daggerok/hands-on-reactive-programming-with-java-12)
+
+## resources
+
+* https://www.udemy.com/course/hands-on-reactive-programming-with-java-12/
---
+++ src/test/java/daggerok/_01/SubscriptionTest.java
@@ -0,0 +1,56 @@
+package daggerok._01;
+
+import com.jayway.awaitility.Duration;
+import lombok.Cleanup;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SubscriptionTest {
+
+  @Test
+  void test() {
+    // given:
+    @Cleanup var publisher = new SubmissionPublisher<String>();
+    var atomicInteger = new AtomicInteger();
+    var subscriber = new Flow.Subscriber<String>() {
+      @Override
+      public void onSubscribe(Flow.Subscription subscription) {
+        System.out.printf("on subscribe: %s%n", subscription);
+        subscription.request(Integer.MAX_VALUE); // backpressure should comes here...
+      }
+
+      @Override
+      public void onNext(String item) {
+        System.out.printf("on next: %s%n", item);
+        atomicInteger.incrementAndGet();
+      }
+
+      @Override
+      public void onError(Throwable throwable) {
+        System.out.printf("error: %s%n", throwable.getLocalizedMessage());
+      }
+
+      @Override
+      public void onComplete() {
+        System.out.printf("done!%n");
+      }
+    };
+
+    // when:
+    publisher.subscribe(subscriber);
+    List.of("ololo", "trololo")
+        .forEach(publisher::submit);
+    publisher.close();
+
+    // then:
+    await().atMost(Duration.TWO_SECONDS)
+           .until(() -> assertThat(atomicInteger.get()).isEqualTo(2));
+  }
+}
